89b89ff7a59b21d259e98abc7c5d0a51bd6e3fd6
HamidrezaKmK <hamidrezakamkari@gmail.com>
2024-08-18 00:20:12 -0400

    add topbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,29 @@
 import { useState } from 'react'
 import './App.css'
+import './top-bar.css'
+import './globals.css'
 import OrbitingCircles from './OrbitingCircles';
 
 function App() {
+  const navbar = (
+    <>
+      <div className="logo"><a href='index.html' className="top-bar-link"> My Logo </a></div>
+          <nav className="nav-links">
+              <a href="#home" className="top-bar-link">Home</a>
+              <a href="#about" className="top-bar-link">About</a>
+              <a href="#contact" className="top-bar-link">Contact</a>
+          </nav>
+    </>
+  );
   return (
+    <>
+      <div className="top-bar">
+        {navbar}
+      </div>
       <div className="h-full w-full">
         <OrbitingCircles />
       </div>
+    </>
   )
 }
 
