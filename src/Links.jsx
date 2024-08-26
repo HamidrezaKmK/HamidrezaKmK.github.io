cbb6b758808f1d91b4fafd0762cc26e8957b962e
HamidrezaKmK <hamidrezakamkari@gmail.com>
2024-08-26 09:28:33 -0400

    re-deploy with CV update

--- src/Links.jsx
+++ src/Links.jsx
@@ -40,7 +40,7 @@ const Links = () => {
                         </a>
                     </li>
                     <li>
-                        <a href="./CV.pdf" target="_blank" className="my-side-item">
+                        <a href="./CV_long.pdf" target="_blank" className="my-side-item">
                             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="#7851A9" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><path d="M14 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V8z"></path><polyline points="14 2 14 8 20 8"></polyline><line x1="16" y1="13" x2="8" y2="13"></line><line x1="16" y1="17" x2="8" y2="17"></line><polyline points="10 9 9 9 8 9"></polyline></svg>
                             Curriculum Vitae
                         </a>
