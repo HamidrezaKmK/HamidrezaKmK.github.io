bce4b6ba342b8b2f90e4ffd4abe6489adf873808
HamidrezaKmK <hamidrezakamkari@gmail.com>
2026-06-12 01:26:41 -0400

    update lockedin

--- src/content/projects.js
+++ src/content/projects.js
@@ -2,8 +2,8 @@ export default [
   {
     "title": "Locked-In",
     "description": "An AI-powered hub for researchers to manage literature, brainstorm insights, and stay 'locked in' via a seamless Slack-integrated workflow.",
-    "href": "https://github.com/HamidrezaKmK",
-    "image": "/projects.png",
+    "href": "https://github.com/HamidrezaKmK/lockedin",
+    "image": "/media/891399-1781241973545.png",
     "tags": [
       "Agents",
       "Acedmia",
